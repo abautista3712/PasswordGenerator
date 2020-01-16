a287d37a886b21a54a27f5f72a9653538877a3ec
Abelard Bautista <abelardbautista@gmail.com>
2020-01-16 15:13:29 -0800

    Added inputLength to check user input in browser against existing length limitations functions.

--- script.js
+++ script.js
@@ -69,35 +69,35 @@ var specialCharArr = [
 ];
 var CharArr = [lowerCaseArr, upperCaseArr, numCharArr, specialCharArr];
 // -> for user input prompt
-var userInputLength = prompt("Please select a password length between 8-128");
+var inputLength = document.querySelector("#inputLength");
 
 // Functions
 // -> for lower and upper limits on password character length
 function passLengthLimit() {
-  if (parseInt(userInputLength) < 8) {
+  if (parseInt(inputLength.value) < 8) {
     alert("Please select a password between 8-128 characters");
-  } else if (parseInt(userInputLength) > 128) {
+  } else if (parseInt(inputLength.value) > 128) {
     alert("Please select a password between 8-128 characters");
   }
 }
 // -> for linking random number generator with 4 character types
 function rNGLowerCase() {
-  for (i = 0; i < userInputLength; i++) {
+  for (i = 0; i < inputLength.value; i++) {
     document.write(lowerCaseArr[Math.floor(Math.random() * 26)]);
   }
 }
 function rNGUpperCase() {
-  for (i = 0; i < userInputLength; i++) {
+  for (i = 0; i < inputLength.value; i++) {
     document.write(upperCaseArr[Math.floor(Math.random() * 26)]);
   }
 }
 function rNGNumber() {
-  for (i = 0; i < userInputLength; i++) {
+  for (i = 0; i < inputLength.value; i++) {
     document.write(numCharArr[Math.floor(Math.random() * 10)]);
   }
 }
 function rNGSpecial() {
-  for (i = 0; i < userInputLength; i++) {
+  for (i = 0; i < inputLength.value; i++) {
     document.write(specialCharArr[Math.floor(Math.random() * 33)]);
   }
 }
@@ -105,9 +105,9 @@ function rNGSpecial() {
 // Function Calls
 // -> testing for functionality / not final function call
 document.querySelector("#generatePass").onclick = function() {
-  if (userInputLength < 8) {
+  if (inputLength.value < 8) {
     passLengthLimit();
-  } else if (userInputLength > 128) {
+  } else if (inputLength.value > 128) {
     passLengthLimit();
   } else {
     rNGUpperCase();
