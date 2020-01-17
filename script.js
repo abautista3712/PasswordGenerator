ac77041cdb838617765fa8b0db5136c8139f5b10
Abelard Bautista <abelardbautista@gmail.com>
2020-01-17 12:43:11 -0800

    Created assignCharArr and assignCharValue to randomly assign character based on all four character types.

--- script.js
+++ script.js
@@ -70,6 +70,7 @@ var specialCharArr = [
 var charArr = [lowerCaseArr, upperCaseArr, numCharArr, specialCharArr];
 // -> for user input prompt
 var inputLength = document.querySelector("#inputLength");
+var targetPassword = document.querySelector("#password");
 
 // Functions
 // -> for lower and upper limits on password character length
@@ -81,38 +82,58 @@ function passLengthLimit() {
   }
 }
 // -> for linking random number generator with 4 character types
+// i) Create variable to save generated password: var password = function()
+// ii) Print password variable to page
+// Fix for loop variables
+function assignCharArr() {
+  for (i = 0; i < 1; i++) {
+    return charArr[Math.floor(Math.random() * 4)];
+  }
+}
+
+assignCharValue();
+
+function assignCharValue() {
+  for (j = 0; j < assignCharArr().length; j++) {
+    console.log(
+      assignCharArr()[Math.floor(Math.random() * assignCharArr().length)]
+    );
+  }
+}
+
 function rNGLowerCase() {
   for (i = 0; i < inputLength.value; i++) {
-    document.write(lowerCaseArr[Math.floor(Math.random() * 26)]);
+    targetPassword.textContent = lowerCaseArr[Math.floor(Math.random() * 26)];
+    // console.log(lowerCaseArr[Math.floor(Math.random() * 26)]);
   }
 }
 function rNGUpperCase() {
   for (i = 0; i < inputLength.value; i++) {
-    document.write(upperCaseArr[Math.floor(Math.random() * 26)]);
+    targetPassword.textContent = upperCaseArr[Math.floor(Math.random() * 26)];
   }
 }
 function rNGNumber() {
   for (i = 0; i < inputLength.value; i++) {
-    document.write(numCharArr[Math.floor(Math.random() * 10)]);
+    targetPassword.textContent = numCharArr[Math.floor(Math.random() * 10)];
   }
 }
 function rNGSpecial() {
   for (i = 0; i < inputLength.value; i++) {
-    document.write(specialCharArr[Math.floor(Math.random() * 33)]);
+    targetPassword.textContent = specialCharArr[Math.floor(Math.random() * 33)];
   }
 }
 
 // Function Calls
 // -> testing for functionality / not final function call
-document.querySelector("#generatePass").onclick = function() {
-  if (inputLength.value < 8) {
-    passLengthLimit();
-  } else if (inputLength.value > 128) {
-    passLengthLimit();
-  } else {
-    rNGUpperCase();
-    rNGLowerCase();
-    rNGNumber();
-    rNGSpecial();
-  }
-};
+// document.querySelector("#generatePass").addEventListener("click", function() {
+//   if (inputLength.value < 8) {
+//     passLengthLimit();
+//   } else if (inputLength.value > 128) {
+//     passLengthLimit();
+//   } else {
+//     rNGLowerCase();
+// rNGUpperCase();
+// rNGNumber();
+// rNGSpecial();
+//   }
+// });
